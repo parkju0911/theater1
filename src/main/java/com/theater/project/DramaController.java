073c1fba418c05f98a988e67eb95d528cbaa7107
koeunseo <eseo128@naver.com>
2018-01-16 13:02:26 +0900

    commit

--- src/main/java/com/theater/project/DramaController.java
+++ src/main/java/com/theater/project/DramaController.java
@@ -1,16 +1,21 @@
 package com.theater.project;
 
-import java.sql.Date;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -18,11 +23,10 @@ import com.theater.drama.DramaDTO;
 import com.theater.drama.DramaListDTO;
 import com.theater.drama.DramaService;
 import com.theater.drama.SeatDTO;
+import com.theater.file.FileDTO;
 import com.theater.member.CompanyDTO;
-import com.theater.member.MemberDTO;
 import com.theater.member.MemberService;
 import com.theater.qna.Qna_viewDTO;
-import com.theater.review.ReviewDTO;
 import com.theater.util.ListData;
 
 @Controller
@@ -33,20 +37,39 @@ public class DramaController {
 	@Inject
 	public MemberService memberService;
 	
-	//좌석 선택 1-15 수정
-	@RequestMapping(value="insertBuy")
-	public void insertBuy(SeatDTO seatDTO, String drama_date, int price, HttpSession session) throws Exception{
-		MemberDTO memberDTO = (MemberDTO)session.getAttribute("member");
-
-		seatDTO.setDrama_date(Date.valueOf(drama_date));;
-		int result=dramaService.insertBuy(seatDTO, memberDTO, price);
-			
-		if(result>0) {
-			System.out.println("성공");
-		}else {
-			System.out.println("실패");
-		}
+	
+	
+@RequestMapping(value="checkCookie", method=RequestMethod.GET)
+	public String testCookie(HttpServletResponse response) throws Exception{
+	Cookie setCookie = new Cookie("name", URLEncoder.encode("dfdf", "UTF-8")); // 쿠키 생성
+/*	Cookie cookie = new Cookie(name, URLEncoder.encode(value, "UTF-8"));*/
+	
+	setCookie.setMaxAge(60*60*24); // 기간을 하루로 지정
+	response.addCookie(setCookie);
+return "sss";
 	}
+	
+	@RequestMapping(value="chkRecent", produces = "application/json; charset=utf8", method=RequestMethod.GET)
+	@ResponseBody
+	public String[] testCookie(HttpServletRequest request) throws Exception{
+		Cookie[] getCookie = request.getCookies();
+		/*String value = URLDecoder.decode(cookie.getValue(), "UTF-8");
+		[출처] 쿠키 값에 한글 값을 세팅하여 넘길시 발생 하는 에러|작성자 제이홀릭*/
+		String value="";
+		String [] values=null;
+			for(int i=0; i<getCookie.length; i++){
+				Cookie c = getCookie[i];
+				String name = c.getName(); // 쿠키 이름 가져오기
+				if(name.equals("title")){
+					
+					value = URLDecoder.decode(c.getValue(),"UTF-8"); // 쿠키 값 가져오기
+					values=value.split(",");
+					
+				}
+			}
+			return values;
+	}
+	
 	//리뷰 리스트 , 별점,별점 평균점수
 	@RequestMapping(value="reviewlist")
 	public ModelAndView review_list(ModelAndView mv , ListData listData , int drama_num)throws Exception{
@@ -58,64 +81,44 @@ public class DramaController {
 		return mv;
 	}
 	
-	//Select Seat 1-15 수정
+	//Select Seat
 	@RequestMapping(value="selectSeat")
 	public ModelAndView selectSeat(SeatDTO seatDTO,ModelAndView mv) throws Exception{
 		//공연 정보 가져오기
 		DramaDTO dramaDTO = dramaService.selectOne(seatDTO.getDrama_num());
+
 		//회사명,좌석 수 가져오기
 		CompanyDTO companyDTO = memberService.searchCompany(dramaDTO.getCompany_num());
 		companyDTO.setName(memberService.searchCompanyName(companyDTO.getId()));
 			
 		//해당 공연 선택된 좌석 리스트 가져오기
 		int date_num= dramaService.search_dateNum(seatDTO.getDrama_num(), seatDTO.getDrama_date().toString(), seatDTO.getDrama_time());
-		List<SeatDTO> selectSeat=dramaService.selectSeat(seatDTO.getDrama_num(), date_num);
-		ArrayList<String> selectSeatDTO = new ArrayList<String>();
-		
-		for(SeatDTO SeatDTO : selectSeat) {
-			String[] split = SeatDTO.getSelect_seat().split(",");
-			for(int i=0; i<split.length; i++) {
-				selectSeatDTO.add(split[i]);
-			}
-		}
-			
-		int total_selectTicket = 0;
-		if(date_num != 0) {
-			for(int i=0; i<selectSeat.size(); i++) {
-				total_selectTicket =+ selectSeat.get(i).getTicket_numbers();
-			}
-		}
-			
+		List<SeatDTO> selectSeatDTO=dramaService.selectSeat(seatDTO.getDrama_num(), date_num);
+		int selectSize = selectSeatDTO.size();
 		mv.addObject("drama", dramaDTO);
 		mv.addObject("company", companyDTO);
 		mv.addObject("seat", seatDTO);
 		mv.addObject("select", selectSeatDTO);
-		mv.addObject("selectSize", total_selectTicket);
+		mv.addObject("selectSize", selectSize);
 		mv.setViewName("drama/selectSeat");
-				
+			
 		return mv;
 	}
-	//Drama_ticket 1-10 수정
+	//Drama_ticket
 	@RequestMapping("dramaTicket")
 	public ModelAndView drama_ticket(DramaListDTO dramaListDTO, ModelAndView mv) throws Exception{
 		//해당 날짜의 해당 시간의 남아있는 좌석 수만큼 가져오기
 		int total_seat = dramaService.total_seat(dramaListDTO.getDrama_num());
 		int date_num = dramaService.search_dateNum(dramaListDTO.getDrama_num(), dramaListDTO.getDrama_date(), dramaListDTO.getDrama_time());
-			
 		List<SeatDTO> select = dramaService.selectSeat(dramaListDTO.getDrama_num(), date_num);
 			
-		int select_size=0;
-		int total_selectTicket = 0;
+		int select_size = 0;
 		if(date_num != 0) {
-			for(int i=0; i<select.size(); i++) {
-				total_selectTicket =+ select.get(i).getTicket_numbers();
-			}
-			select_size = total_seat-total_selectTicket;
+			select_size = total_seat-select.size();
 		}
-			
 		mv.addObject("ticket", select_size);
 		mv.setViewName("drama/drama_ticket");
-				
+			
 		return mv;
 	}
 	//Drama_time list
@@ -123,34 +126,61 @@ public class DramaController {
 	public ModelAndView drama_time(DramaListDTO dramaListDTO, ModelAndView mv) throws Exception{
 		//해당 날짜 공연시간 가져오기
 		List<DramaListDTO> ar = dramaService.timeList(dramaListDTO.getDrama_num(), dramaListDTO.getDrama_date());
-				
+			
 		mv.addObject("time", ar);
 		mv.addObject("view", dramaListDTO);
 		mv.setViewName("drama/drama_time");
-				
+			
 		return mv;
 	}
 	//Drama View
 	@RequestMapping(value="dramaview")
-	public ModelAndView selectOne(int drama_num, ModelAndView mv,RedirectAttributes rd) throws Exception{
+	public ModelAndView selectOne(int drama_num, ModelAndView mv,RedirectAttributes rd,HttpServletRequest request, HttpServletResponse response) throws Exception{
 		//공연 정보가져오기
 		DramaDTO dramaDTO = dramaService.selectOne(drama_num);
 		//해당 공연의 날짜 정보 가져오기
 		List<DramaListDTO> ar = dramaService.dramaList(drama_num);
-		//후기(합계) 숫자 표시
-		int totalcount =dramaService.totalcount(drama_num);
-		
-		//연극 리뷰 최신꺼 보여주기 안됨 대기
-	/*	ReviewDTO selectOne_review= dramaService.selectOne_review(drama_num);*/
-		
-		List<ReviewDTO> ar_review = dramaService.selectList_review(drama_num);
-		
+				
 		if(dramaDTO != null) {
+			
+			//중복제거
+			String [] strings=null;
+			boolean check=true;
+			boolean newCheck=true;
+			String result="";
+			Cookie [] cookies = request.getCookies();
+			for (Cookie cookie : cookies) {
+				
+				if(cookie.getName().equals("title")){
+					strings = cookie.getValue().split(",");
+					for(String s : strings){
+						if(s.equals(String.valueOf(dramaDTO.getDrama_num()))){
+							check=false;
+						}
+					}
+					if(check){
+						//중복데이터 X
+						result=cookie.getValue();
+						result=result+","+dramaDTO.getDrama_num();
+						Cookie c = new Cookie("title", result);
+						response.addCookie(c);
+					}
+					newCheck=false;
+					break;
+				
+				}
+				//if
+				
+			}//for
+			
+			if(newCheck){
+				Cookie c = new Cookie("title", String.valueOf(dramaDTO.getDrama_num()));
+				response.addCookie(c);
+			}
+			//쿠키 파싱후 추가
+			//중복제거 끝
 			mv.addObject("view", dramaDTO);
 			mv.addObject("list", ar);
-			mv.addObject("review", ar_review);
-			mv.addObject("total", totalcount); 
-			/*mv.addObject("reviewOne", selectOne_review);*/
 			mv.setViewName("drama/dramaview");
 		}else {
 			rd.addFlashAttribute("message", "잘못된 접근방식 입니다.");
@@ -159,7 +189,6 @@ public class DramaController {
 				
 		return mv;
 	}
-	
 	//환불규정 page
 	@RequestMapping(value="refundlist")
 	public String refundlist()throws Exception{
@@ -216,25 +245,48 @@ public class DramaController {
 		mv = dramaService.dramaReviewList(listData);
 		return mv;
 	}
-	@RequestMapping(value="dramaReviewview")
-		public ModelAndView dramaReviewview(ModelAndView mv, int review_num)throws Exception{
 		
-		ReviewDTO reviewDTO= dramaService.review_selectOne(review_num);
-		mv.addObject("selectOne", reviewDTO);
-		mv.setViewName("drama/dramaReviewview");
-		
-		return mv;
-	}
 	//selectList
 	@RequestMapping(value="dramaList")
-	public ModelAndView selectList(ListData listData) throws Exception {
+	public ModelAndView selectList(ListData listData, HttpServletRequest request) throws Exception {
 		ModelAndView mv = null;
 		mv = dramaService.selectList(listData);
 		mv.setViewName("drama/list");
+	
+		Cookie [] cookies = request.getCookies();
+		List<DramaDTO> ar = new ArrayList<DramaDTO>();
+		for (Cookie cookie : cookies) {
+			cookie.setMaxAge(60*60*24);
+			if(cookie.getName().equals("title")){
+				String [] strings = cookie.getValue().split(",");
+				for (String string : strings) {
+					
+					DramaDTO dramaDTO=dramaService.selectOne(Integer.parseInt(string));
+					ar.add(dramaDTO);
+				}
+				mv.addObject("title", ar);
+				
+			}
+		}
 		
 		return mv;
 	}
 	
+	
+	//Drama_file list
+		@RequestMapping(value="dramaFile")
+		public ModelAndView drama_file(FileDTO fileDTO, ModelAndView mv) throws Exception{
+			//해당 날짜 공연시간 가져오기
+			/*List<FileDTO> ar = dramaService.fileList(fileDTO.getFile_num(), fileDTO.getFile_name());
+				
+			mv.addObject("file", ar);
+			mv.addObject("view", fileDTO);
+			mv.setViewName("drama/drama_file");*/
+				
+			return mv;
+		}
+		
+		
 	//insert -> form 이동
 	@RequestMapping(value="dramaWrite", method=RequestMethod.GET)
 	public String insert(Model model) {
